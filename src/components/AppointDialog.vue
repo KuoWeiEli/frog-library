e15228a5b00b42027e20fcf9b92b95cd77459ad1
eli.huang <naughtyowlkid@gmail.com>
2020-12-24 06:19:46 +0800

    frog-library-4 館藏資訊 預約功能雛形

---
+++ src/components/AppointDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-dialog :visible="appointment.visible" title="申請預約" :show-close="false">
+    <el-form :model="appointmentForm">
+      <el-form-item label="書名" :label-width="formLabelWidth">
+        <el-input v-model="appointment.bookData.bookName" autocomplete="off" readonly></el-input>
+      </el-form-item>
+      <el-form-item label="申請人" :label-width="formLabelWidth">
+        <el-input v-model="appointmentForm.applicant" autocomplete="off" readonly></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="appointment.visible = false">取消預約</el-button>
+      <el-button type="primary" @click="appointment.visible = false">確認預約</el-button>
+    </div>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'appoint-dialog',
+  props: {
+    appointment: {
+      visible: Boolean,
+      bookData: {},
+      empData: {}
+    }
+  },
+  data() {
+    return {
+      formLabelWidth: '120px',
+      appointmentForm: {
+        applicant: this.appointment.empData.empId + '-' + this.appointment.empData.empName,
+      }
+    }
+  }
+}
+</script>
--- src/components/LibraryInfo.vue
+++ src/components/LibraryInfo.vue
@@ -1,111 +1,131 @@
 <template>
-  <el-table
-      :data="products"
-      stripe
-      style="width: 100%"
-      height="800"
-      size="small">
-    <el-table-column
-        label="操作"
-    >
-    <template slot-scope="scope">
-      <el-button type="text" @click.native.prevent="alert(scope.row)"
-                 size="small" icon="el-icon-date">預約</el-button>
-    </template>
-    </el-table-column>
-    <el-table-column
-        prop="bookSeq"
-        label="序號"
-        width="100">
-    </el-table-column>
-    <el-table-column
-        prop="bookName"
-        label="書刊名"
-        width="500">
-    </el-table-column>
-    <el-table-column
-        prop="author"
-        label="作者"
-        width="550">
-    </el-table-column>
-    <el-table-column
-        prop="tech"
-        label="技術"
-        width="250">
-    </el-table-column>
-    <el-table-column
-        prop="publisher"
-        label="出版社"
-        width="80">
-    </el-table-column>
-    <el-table-column
-        prop="publishDate"
-        label="出版年"
-        width="100">
-    </el-table-column>
-    <el-table-column
-        prop="empId"
-        label="借閱者員編"
-        width="90">
-    </el-table-column>
-    <el-table-column
-        prop="empName"
-        label="借閱者"
-        width="80">
-    </el-table-column>
-<!--    <el-table-column-->
-<!--        prop="getDateRange"-->
-<!--        label="領取起訖日"-->
-<!--        width="180">-->
-<!--    </el-table-column>-->
-    <el-table-column
-        prop="bollowDateRange"
-        label="借閱起訖日"
-        width="100">
-    </el-table-column>
-<!--    <el-table-column-->
-<!--        prop="returnDate"-->
-<!--        label="歸還日期"-->
-<!--        width="180">-->
-<!--    </el-table-column>-->
-    <el-table-column
-        prop="bookStatus"
-        label="狀態"
-        width="80">
-    </el-table-column>
-    <el-table-column
-        prop="waitNum"
-        label="預約等待人數"
-        width="110">
-    </el-table-column>
-  </el-table>
+
+  <div>
+    <el-table
+        :data="libraryInfo"
+        stripe
+        style="width: 100%"
+        height="800"
+        size="small">
+      <el-table-column
+          label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button type="text" @click.native.prevent="appoint(scope.row)"
+                     size="small" icon="el-icon-date">預約
+          </el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+          prop="bookSeq"
+          label="序號"
+          width="100">
+      </el-table-column>
+      <el-table-column
+          prop="bookName"
+          label="書刊名"
+          width="500">
+      </el-table-column>
+      <el-table-column
+          prop="author"
+          label="作者"
+          width="550">
+      </el-table-column>
+      <el-table-column
+          prop="tech"
+          label="技術"
+          width="250">
+      </el-table-column>
+      <el-table-column
+          prop="publisher"
+          label="出版社"
+          width="80">
+      </el-table-column>
+      <el-table-column
+          prop="publishDate"
+          label="出版年"
+          width="100">
+      </el-table-column>
+      <el-table-column
+          prop="empId"
+          label="借閱者員編"
+          width="90">
+      </el-table-column>
+      <el-table-column
+          prop="empName"
+          label="借閱者"
+          width="80">
+      </el-table-column>
+      <!--    <el-table-column-->
+      <!--        prop="getDateRange"-->
+      <!--        label="領取起訖日"-->
+      <!--        width="180">-->
+      <!--    </el-table-column>-->
+      <el-table-column
+          prop="bollowDateRange"
+          label="借閱起訖日"
+          width="100">
+      </el-table-column>
+      <!--    <el-table-column-->
+      <!--        prop="returnDate"-->
+      <!--        label="歸還日期"-->
+      <!--        width="180">-->
+      <!--    </el-table-column>-->
+      <el-table-column
+          prop="bookStatus"
+          label="狀態"
+          width="80">
+      </el-table-column>
+      <el-table-column
+          prop="waitNum"
+          label="預約等待人數"
+          width="110">
+      </el-table-column>
+    </el-table>
+
+    <appoint-dialog :appointment="appointment"/>
+  </div>
+
+
 </template>
 
 <script>
 import axios from "axios";
+import AppointDialog from "@/components/AppointDialog";
 
 export default {
   name: 'LibraryInfo',
+  components: {AppointDialog},
   created() {
-    this.getProducts()
+    this.getLibraryInfo()
   },
   data() {
     return {
-      products: []
+      libraryInfo: [],
+      appointment: {
+        visible: false,
+        bookData: {},
+        empData: {
+          empId: '0400',
+          empName: '黃國維'
+        }
+      }
     }
   },
   methods: {
-    async getProducts() {
-      console.log('getProducts.....')
-      axios.get('http://localhost:3000/products')
+    async getLibraryInfo() {
+      console.log('getLibraryInfo.....')
+      axios.get('http://localhost:3000/libraryInfo')
           .then(response => {
-            this.products = response.data
+            this.libraryInfo = response.data
           })
+    },
+    appoint(row) {
+      console.log(row)
+      this.appointment.bookData = row
+      this.appointment.visible = true
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
