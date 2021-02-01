73020c618cce0a210125a94a537c08295b1b291f
eli <naughtyowlkid@gmail.com>
2021-02-01 22:13:15 +0800

    AWS-Amplify files

---
+++ src/graphql/queries.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getBook = /* GraphQL */ `
+  query GetBook($id: ID!) {
+    getBook(id: $id) {
+      id
+      name
+      author
+      tech
+      publisher
+      publishDate
+      status
+      createDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listBooks = /* GraphQL */ `
+  query ListBooks(
+    $filter: ModelBookFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listBooks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        author
+        tech
+        publisher
+        publishDate
+        status
+        createDate
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBook = /* GraphQL */ `
+  subscription OnCreateBook {
+    onCreateBook {
+      id
+      name
+      author
+      tech
+      publisher
+      publishDate
+      status
+      createDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBook = /* GraphQL */ `
+  subscription OnUpdateBook {
+    onUpdateBook {
+      id
+      name
+      author
+      tech
+      publisher
+      publishDate
+      status
+      createDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBook = /* GraphQL */ `
+  subscription OnDeleteBook {
+    onDeleteBook {
+      id
+      name
+      author
+      tech
+      publisher
+      publishDate
+      status
+      createDate
+      createdAt
+      updatedAt
+    }
+  }
+`;
